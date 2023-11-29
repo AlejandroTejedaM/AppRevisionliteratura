ade44c43728433d152ccb1d9c6783b3a9f2029bb
CARLOS <carlosjconde593@gmail.com>
2023-11-29 10:53:24 -0600

    Creacion de arreglos de sustantivos, adverbios, verbos y adjetivos

--- AppRevisionliteratura/FrmLiteratura.Designer.cs
+++ AppRevisionliteratura/FrmLiteratura.Designer.cs
@@ -28,18 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            txtPalabra = new TextBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(304, 256);
+            button1.Name = "button1";
+            button1.Size = new Size(296, 115);
+            button1.TabIndex = 0;
+            button1.Text = "btn_Validar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtPalabra
+            // 
+            txtPalabra.Location = new Point(304, 194);
+            txtPalabra.Name = "txtPalabra";
+            txtPalabra.Size = new Size(296, 31);
+            txtPalabra.TabIndex = 1;
             // 
             // FrmLiteratura
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1001, 519);
+            ClientSize = new Size(1251, 649);
+            Controls.Add(txtPalabra);
+            Controls.Add(button1);
+            Margin = new Padding(4);
             Name = "FrmLiteratura";
             Text = "Caperucita Rojaa";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox txtPalabra;
     }
 }
--- AppRevisionliteratura/FrmLiteratura.cs
+++ AppRevisionliteratura/FrmLiteratura.cs
@@ -1,10 +1,65 @@
+using System.Collections.Generic;
+using System.Windows.Forms;
+
 namespace AppRevisionliteratura
 {
     public partial class FrmLiteratura : Form
     {
+        String[] Sustantivos;
+        String[] Abverbios;
+        String[] Verbos;
+        String[] Adjetivos;
+
+
         public FrmLiteratura()
         {
             InitializeComponent();
+            Abverbios = new string[] { "Rápidamente", "Sigilosamente", "Astutamente",
+                "Dulcemente", "Cautamente", "Gentilmente", "Bruscamente", "Hábilmente",
+                "Furiosamente", "Ingenuamente","Temprano", "Pronto", "Nunca", "Siempre",
+                "Lentamente", "Velozmente", "Despacio", "Urgentemente", "Sorpresivamente",
+                "Furtivamente","Lejos", "Profundamente", "Aquí", "Allí", "Cerca",
+                "Adentro", "Afuera", "Mansamente", "Tiernamente", "Furtivamente" };
+
+            Verbos = new string[] { "Ir", "Encontrar", "Hablar", "Comer", "Llegar", "Abrir",
+              "Correr", "Salvar", "Asustar", "Ver", "Vestir", "Dormir", "Engañar",
+              "Caminar", "Preguntar", "Recoger", "Llevar", "Llamar", "Poner", "Decir" };
+
+            Sustantivos = new string[] { "Abuelita", "Caperucita", "Lobo", "Leñador", "Bosque", "Cabaña",
+             "Capa", "Canasta", "Camino", "Flores", "Árboles", "Ramas", "Ojos", "Orejas",
+             "Dientes", "Colmillos", "Garras", "Ventana", "Cama", "Silla", "Bosque", "Sendero",
+             "Picnic", "Pasto", "Patas", "Nariz", "Sol", "Nubes", "Hacha", "Chimenea" };
+
+            Adjetivos = new string[] { "colorido", "caprichoso", "infantil", "juguetón", "alegre", "soleado",
+            "pacífico", "pintoresco", "rural", "idílico", "agradable", "encantador", "bucólico", "relajante",
+            "tranquilo", "nostálgico", "romántico", "pastoral", "divertida", "caricaturesca", "festivo", "vibrante",
+            "animado", "sereno", "jubiloso", "acogedor", "exuberante", "armonioso", "resplandeciente", "sugestivo",
+            "radiante", "plácido", "sosegado", "risueño", "paradisíaco" };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string palabra = txtPalabra.Text.ToLower();
+            if (Abverbios.Contains(palabra))
+            {
+                MessageBox.Show("La palabra es un adverbio.");
+            }
+            else if (Verbos.Contains(palabra))
+            {
+                MessageBox.Show("La palabra es un verbo.");
+            }
+            else if (Sustantivos.Contains(palabra))
+            {
+                MessageBox.Show("La palabra es un sustantivo.");
+            }
+            else if (Adjetivos.Contains(palabra))
+            {
+                MessageBox.Show("La palabra es un adjetivo.");
+            }
+            else
+            {
+                MessageBox.Show("La palabra no es ni adverbio, ni verbo, ni sustantivo, ni adjetivo.");
+            }
         }
     }
 }
