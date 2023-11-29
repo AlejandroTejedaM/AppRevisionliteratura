74e6b373c9fcccc491ce056c07b3344e86ec3bc1
Eduardo Isidro Hernandez <eduardoisidroh13@gmail.com>
2023-11-29 08:50:14 -0600

    cambie el nombre del frm

--- AppRevisionliteratura/FrmLiteratura.Designer.cs
+++ AppRevisionliteratura/FrmLiteratura.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // FrmLiteratura
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1001, 519);
+            Name = "FrmLiteratura";
+            Text = "Caperucita Roja";
+            ResumeLayout(false);
         }
 
         #endregion
